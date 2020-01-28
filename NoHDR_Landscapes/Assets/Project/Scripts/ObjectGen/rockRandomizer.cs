ae206607d12bdfd6606bc7757847dcabec3d1a4a
LorenzoHKU <lorenzohartman2000@gmail.com>
2020-01-28 15:05:28 +0100

    Initial Unity Commit.

---
+++ NoHDR_Landscapes/Assets/Project/Scripts/ObjectGen/rockRandomizer.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class rockRandomizer : MonoBehaviour
+{
+
+    public int seed = 37823787;
+    private Material mat;
+    private List<Vector3> vertices = new List<Vector3>();
+    private List<Vector3> doneVerts = new List<Vector3>();
+    private Vector3 center;
+
+
+    void Start()
+    {
+        Random.InitState(seed);
+        float offset = Random.Range(0, 20);
+
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        for (int s = 0; s < mesh.vertices.Length; s++)
+        {
+            vertices.Add(mesh.vertices[s]);
+        }
+
+        center = GetComponent<Renderer>().bounds.center;
+
+        for (int v = 0; v < vertices.Count; v++)
+        {
+            bool used = false;
+            for (int k = 0; k < doneVerts.Count; k++)
+            {
+                if (doneVerts[k] == vertices[v])
+                {
+                    used = true;
+                }
+            }
+            if (!used)
+            {
+                Vector3 curVector = vertices[v];
+                doneVerts.Add(curVector);
+                int smoothing = Random.Range(7, 9);
+                Vector3 changedVector = (curVector + ((curVector - center) * (Mathf.PerlinNoise((float)v / offset, (float)v / offset) / smoothing)));
+                for (int s = 0; s < vertices.Count; s++)
+                {
+                    if (vertices[s] == curVector)
+                    {
+                        vertices[s] = changedVector;
+                    }
+                }
+            }
+        }
+
+        mesh.SetVertices(vertices);
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+    }
+}
---
+++ NoHDR_Landscapes/Assets/Project/Scripts/RandomizeRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //RandomizeMyRotation();
+        RandomizeYRotation();
+    }
+
+    //void RandomizeMyRotation()
+    //{
+    //    transform.rotation = Random.rotation;
+    //}
+
+    void RandomizeYRotation()
+    {
+        Quaternion randYRotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+        transform.rotation = randYRotation;
+    }
+
+}
